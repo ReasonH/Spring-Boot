5a9a2d17128ee6379bb1926f11289590ef9bff7f
YooHyuk Lee <azsxd34@naver.com>
2020-02-19 00:20:20 +0900

    lec05: applying the security setting to existing test

--- src/main/java/com/reason/application/springboot/Application.java
+++ src/main/java/com/reason/application/springboot/Application.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication // 스프링 부트 자동 설정, Bean읽기 생성 자동화
 public class Application { // 메인 클래스 - 이 클래스는 항상 프로젝트의 최상단에 위치해야함
     public static void main(String[] args) {
