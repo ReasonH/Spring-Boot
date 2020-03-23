a37c4a89e1933492fcb3a2fbb56f36ce15ed059e
YooHyuk Lee <azsxd34@naver.com>
2020-03-24 02:36:48 +0900

    final deploy test

--- scripts/start.sh
+++ scripts/start.sh
@@ -12,7 +12,7 @@ echo "> cp $REPOSITORY/zip/jar.* $REPOSITORY/"
 cp $REPOSITORY/zip/jar.* $REPOSITORY/
 
 echo "> 새 에플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/zip/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
